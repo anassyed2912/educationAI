c297c08a9684ba922036b0276cc2095a47e95605
anassyed2912 <115107117+anassyed2912@users.noreply.github.com>
2024-06-30 14:11:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
   },
 });
